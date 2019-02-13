64f530cfbf3134bbd71099d8950b5fdd264f54bb
admin <admin@13a>
2019-02-13 12:58:58 +0700

    commit ke 3

---
+++ retrieval/src/model/Posting.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class Posting {
+
+    private Document document;
+
+    public Posting(Document document) {
+        this.document = document;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+}
